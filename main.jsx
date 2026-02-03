1f2ca347b45f273194033d245c4eddfe8ec797ca
Santanu Samanta <samanta.biswajit@s.amity.edu>
2026-02-03 23:42:33 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,19 +1,15 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import App from './App.jsx'
+import './index.css'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  throw new Error('Missing VITE_CLERK_PUBLISHABLE_KEY')
 }
-
-
-
-
-
 
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
@@ -22,3 +18,4 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </ClerkProvider>
 )
+
